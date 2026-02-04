b042d44ee5eee27e09e3d69063b1a02b2ab0de45
Dženo <dzeno@Dzenos-MacBook-Pro.local>
2026-02-04 09:09:29 +0100

    fixing bug for macpro m4 14'

--- init.lua
+++ init.lua
@@ -683,15 +683,15 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {
-          gofumpt = true,
-          usePlaceholders = true,
-          completeUnimported = true,
-          staticcheck = true,
-        },
-        templ = {},
-        tailwindcss = {},
-        eslint = {},
+        -- gopls = {
+        -- gofumpt = true,
+        -- usePlaceholders = true,
+        -- completeUnimported = true,
+        -- staticcheck = true,
+        -- },
+        -- templ = {},
+        -- tailwindcss = {},
+        -- eslint = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -962,7 +962,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'templ', 'python' },
